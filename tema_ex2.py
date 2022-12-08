65e8bdf53d23ea9d9c0a0916c67e3b2874dd1a69
AdrianaVlad <100144132+AdrianaVlad@users.noreply.github.com>
2022-12-08 15:29:15 +0200

    Add files via upload

--- tema_ex2.py
+++ tema_ex2.py
@@ -1,8 +1,9 @@
 import cmath
+from cmath import sqrt
 import numpy as np
 a=[complex(1,0),complex(0,0)]
-b=[complex(1,0),complex(0,0)]
-c=[complex(1,0),complex(0,0)]
+b=[complex(sqrt(0.5),0),complex(sqrt(0.5),0)]
+c=[complex(0,0),complex(1,0)]
 def inmultire(a,b,c):
     A=[[a[0]*b[0]],[a[0]*b[1]],[a[1]*b[0]],[a[1]*b[1]]] #|ab>
     B=[np.conj(c[0]*a[0]),np.conj(c[0]*a[1]),np.conj(c[1]*a[0]),np.conj(c[1]*a[1])] #<ca|
